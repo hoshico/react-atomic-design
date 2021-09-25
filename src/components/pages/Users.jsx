eba14d0fbba56b88945e54d186383018a853b20b
hoshico <77495217+hoshico@users.noreply.github.com>
2021-09-25 17:05:06 +0900

    add Recoil

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,10 +1,12 @@
-import React, { useContext } from "react";
+import React from "react";
 import styled from "styled-components";
-import { UserContext } from "../../providers/UserProvider";
+// import { UserContext } from "../../providers/UserProvider";
 import { SecondaryButton } from "../atoms/button/SecondaryButton";
+import { useRecoilState } from "recoil";
 
 import { SearchInput } from "../moleculese/SeachInput";
 import { UserCard } from "../organisms/user/UserCard";
+import { userState } from "../../store/userStore";
 
 const users = [...Array(10).keys()].map((val) => {
   return {
@@ -21,8 +23,8 @@ const users = [...Array(10).keys()].map((val) => {
 });
 
 export const Users = () => {
-  const { userInfo, setUserInfo } = useContext(UserContext);
-
+  // const { userInfo, setUserInfo } = useContext(UserContext);
+  const [userInfo, setUserInfo] = useRecoilState(userState);
   const onClickSwitch = () => setUserInfo({ isAdmin: !userInfo.isAdmin });
 
   return (
